929007be6e0058986749fd733aac0ba9aea6db9d
Joseph Congdon <jocongdon@yahoo.com>
2021-10-07 09:59:42 -0500

    joins lesson

--- mysql/aliases_exercises.sql
+++ mysql/aliases_exercises.sql
@@ -2,3 +2,36 @@ USE employees;
 
 SELECT CONCAT(emp_no,' - ' ,first_name,' ', last_name) AS full_name , birth_date AS DOB FROM employees
 LIMIT 10;
+
+SELECT CONCAT(title), COUNT(*) FROM titles
+GROUP BY title;
+
+SHOW INDEXES FROm departments;
+
+SHOW INDEXES FROM employees;
+
+
+ALTER TABLE salaries ADD INDEX salary_index (salary);
+
+SELECT salary FROM salaries
+WHERE salary < 700000 AND salary > 50000;
+
+USE codeup_test_db;
+SELECT * FROM quotes;
+
+CREATE TABLE different_quotes (
+    id INT NOT NULL AUTO_INCREMENT,
+    content VARCHAR(240) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+INSERT INTO different_quotes (content, author)
+VALUES ( 'Death is a funny thing. Not funny haha, like a Woody Allen movie, but funny strange, like a Woody Allen marriage.', 'Norm Macdonald');
+
+SHOW INDEXES FROM different_quotes;
+
+ALTER TABLE different_quotes ADD UNIQUE (content, author);
+
+SELECT * FROM different_quotes;
---
+++ mysql/indexes-exercise.sql
@@ -0,0 +1,40 @@
+USE employees;
+
+SHOW INDEXES FROM salaries;
+
+SHOW TABLES;
+
+DESCRIBE departments;
+
+SELECT * FROM departments;
+
+
+DESCRIBE dept_emp;
+
+SELECT * FROM dept_emp;
+
+DESCRIBE dept_manager;
+
+DESCRIBE employees;
+
+DESCRIBE salaries;
+
+DESCRIBE titles;
+
+USE codeup_test_db;
+
+SHOW TABLES;
+
+SELECT * FROM albums;
+
+DESCRIBE albums;
+
+ALTER TABLE albums ADD UNIQUE unique_artists (artist, name);
+
+CREATE TABLE persons (
+                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                         first_name VARCHAR(25) NOT NULL,
+                         album_id INT UNSIGNED NOT NULL,
+                         PRIMARY KEY (id),
+                         FOREIGN KEY (album_id) REFERENCES albums (id)
+);
